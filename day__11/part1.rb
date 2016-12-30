b37b8f64e547083896a11c3683e61afea627d3ec
nicholas lemay <lemaynicholas@gmail.com>
2016-12-30 10:57:20 -0500

    Deleting day 12 content from day 11. Till we meet again day 11.

--- day__11/part1.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'cpu'
-
-@cpu = CPU.new
-puts 'running...'
-@cpu.process_instructions(File.readlines('input.txt'))
-puts "Part 1 result #{@cpu.value_of_register(:a)}"
--- day__11/part2.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'cpu'
-
-@cpu = CPU.new
-puts 'running...'
-@cpu.process_instructions(['cpy 1 c'])
-@cpu.process_instructions(File.readlines('input.txt'))
-puts "Part 2 result #{@cpu.value_of_register(:a)}"
